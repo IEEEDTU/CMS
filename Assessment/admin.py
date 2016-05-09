c6474227d6b21b41bd8f355a5f3250c76af7e5bd
Rahul Arora <rahularora6532@gmail.com>
2016-05-10 01:17:27 +0530

    minor changes in course, assessment and profiler app

--- Assessment/admin.py
+++ Assessment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from Assessment.models import Score, Result
+from Assessment.models import Score, Result, Assignment, AssignmentResponse
 
 # Register your models here.
 admin.site.register(Score)
 admin.site.register(Result)
+admin.site.register(Assignment)
+admin.site.register(AssignmentResponse)
--- Profiler/models/Faculty.py
+++ Profiler/models/Faculty.py
@@ -2,56 +2,66 @@
 from .Person import *
 from Course.models import Department
 
+
 class FacultyManager(models.Manager):
-    def addFaculty(self,request):
+    def addFaculty(self, request):
+        """ adds new faculty member """
         nameObjs = Name.objects.addName(request)
         addressObjs = Address.objects.addAddress(request)
         contactObjs = Mobile.objects.addContacts(request)
         departmentObj = Department.objects.get(deptId=request['deptId'])
 
         F = Faculty(
-            dtuRegId = request["dtuRegId"],
-            name = nameObjs[0],
-            permanentAdd = addressObjs[0],
-            presentAdd = addressObjs[1],
-            guardianAdd = addressObjs[2],
-            personalMobile = contactObjs[0],
-            alternativeMobile = contactObjs[1],
-            personalEmail = request["personalEmail"],
-            alternativeEmail = request["alternativeEmail"],
-            dateOfBirth = request["dateOfBirth"],
-            gender = request["gender"],
-            category = request["category"],
-            nationality = request["nationality"],
-            religion = request["religion"],
-            dormitory = request["dormitory"],
-            fatherName = nameObjs[1],
-            motherName = nameObjs[2],
-            facultyId = request['facultyId'],
-            designation = request['designation'],
-            dateOfJoining = request['dateOfJoining'],
-            jobType = request['jobType'],
-            department = request['department'],
-            libraryId = request['libraryId'],
+            dtuRegId=request["dtuRegId"],
+            name=nameObjs[0],
+            permanentAdd=addressObjs[0],
+            presentAdd=addressObjs[1],
+            guardianAdd=addressObjs[2],
+            personalMobile=contactObjs[0],
+            alternativeMobile=contactObjs[1],
+            personalEmail=request["personalEmail"],
+            alternativeEmail=request["alternativeEmail"],
+            dateOfBirth=request["dateOfBirth"],
+            gender=request["gender"],
+            category=request["category"],
+            nationality=request["nationality"],
+            religion=request["religion"],
+            dormitory=request["dormitory"],
+            fatherName=nameObjs[1],
+            motherName=nameObjs[2],
+            facultyId=request['facultyId'],
+            designation=request['designation'],
+            dateOfJoining=request['dateOfJoining'],
+            jobType=request['jobType'],
+            department=request['department'],
+            libraryId=request['libraryId'],
             password=request['password'],
-            )
+        )
         F.save()
         return F
 
     def getFacultyByRegId(self, request):
-        """ To get the object of student by its dtuRegId """
-        F = Faculty.objects.get(dtuRegId = request["dtuRegId"])
+        """ To get the object of faculty by its dtuRegId """
+        F = Faculty.objects.get(dtuRegId=request["dtuRegId"])
         return F
 
     def getFacultyByFacultyId(self, request):
-        """ To get the object of student by its dtuRegId """
-        F = Faculty.objects.get(facultyId = request["facultyId"])
+        """ To get the object of faculty by its dtuRegId """
+        F = Faculty.objects.get(facultyId=request["facultyId"])
+        return F
+
+    def getFacultyByRegIdOrFacultyId(self, request):
+        """ To get the object of faculty by its dtuRegId or facultyId, whatever specified in request """
+        if "dtuRegId" in request.keys():
+            F = Faculty.objects.get(dtuRegId=request["dtuRegId"])
+        else:
+            F = Faculty.objects.get(facultyId=request["facultyId"])
         return F
 
     def getFacultyByName(self, request):
         """ To get the objects of student by its name """
-        N = Name.objects.get(preferredName = request['preferredName'])
-        F = Faculty.objects.get(name = N)
+        N = Name.objects.get(preferredName=request['preferredName'])
+        F = Faculty.objects.get(name=N)
         # rollNoList = []
         # for n in N:
         #   S = Student.objects.filter(name = n)
@@ -61,23 +71,23 @@ class FacultyManager(models.Manager):
 
     def deleteFaculty(self, request):
         if "dtuRegId" in request.keys():
-            F = Faculty.objects.get(dtuRegId = request['dtuRegId'])
+            F = Faculty.objects.get(dtuRegId=request['dtuRegId'])
         elif "facultyId" in request.keys():
-            F = Faculty.objects.get(facultyId = request['facultyId'])
+            F = Faculty.objects.get(facultyId=request['facultyId'])
         F = F.delete()
         return F
 
-    def retrieveFaculty(self,request):
+    def retrieveFaculty(self, request):
         """ retrieve IDs of all the courses depending on the request """
         """ note: department is must """
         """ other optional attributes: dateOfJoining, jobType """
-        D = Department.objects.get(deptId = request['deptId'])
+        D = Department.objects.get(deptId=request['deptId'])
         objList = Faculty.objects.filter(department=D)
 
         if "dateOfJoining" in request.keys():
             objList = objList.objects.filter(dateOfJoining=request['dateOfJoining'])
         if "jobType" in request.keys():
-            objList = objList.objects.filter(jobType = request['jobType'])
+            objList = objList.objects.filter(jobType=request['jobType'])
 
         idList = []
 
@@ -97,8 +107,8 @@ class Faculty(Person):
     # Job Type (E.g.: Permanent, Guest)
     jobType = models.CharField(max_length=50, blank=False, null=False)
     # Department
-    department = models.ForeignKey(Department, on_delete = models.CASCADE, default=False)
-	# Salary
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=False)
+    # Salary
     # salary = models.ForeignKey(Salary, on_delete=models.CASCADE)
     # Library ID
     libraryId = models.CharField(max_length=30, blank=False, null=False)
@@ -109,4 +119,3 @@ class Faculty(Person):
 
     def __str__(self):
         return self.facultyId + " - " + str(self.name)
-
