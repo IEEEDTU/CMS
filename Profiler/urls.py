2a0f1610a007fa2327e617148ccf991ef82f24e3
Rohitjain30 <bestrohit05@gmail.com>
2016-05-28 14:40:01 +0530

    Update urls.py

--- Profiler/urls.py
+++ Profiler/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	url(r'^addStudent', views.addStudent, name='addStudent'),
 	url(r'^editStudent', views.editStudent, name='editStudent'),
 	url(r'^deleteStudent', views.deleteStudent, name='deleteStudent'),
+	url(r'^retrieveProjects', views.retrieveProjects, name='retrieveProjects'),
 	url(r'^getStudentByRollNo', views.getStudentByRollNo, name='getStudentByRollNo'),
 	
 	url(r'^addFaculty', views.addFaculty, name='addFaculty'),
