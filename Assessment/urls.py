4895a8e51b2fc7d04042c31e5c66d3395edca95f
Rohitjain30 <bestrohit05@gmail.com>
2016-06-02 00:17:46 +0530

    Update urls.py

--- Assessment/urls.py
+++ Assessment/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^getAssignmentByCode', views.getAssignmentByCode, name='getAssignmentByCode'),
+    url(r'^getAssignmentsByCourse', views.getAssignmentsByCourse, name='getAssignmentsByCourse'),
     url(r'^retrieveAssignments', views.retrieveAssignments, name='retrieveAssignments'),
+    url(r'^retrieveAssignmentByBranch', views.retrieveAssignmentByBranch, name='retrieveAssignmentByBranch'),
+    url(r'^retrieveAssignmentResponsesByStudent', views.retrieveAssignmentResponses, name='retrieveAssignmentResponses'),
+    url(r'^getResultsByStudent', views.getResultsByStudent, name='getResultsByStudent'),
 
 ]
